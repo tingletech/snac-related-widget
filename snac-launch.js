2b8c3791fe0e30b227e755db277af4a3f0d81949
Brian Tingle <brian.tingle.cdlib.org@gmail.com>
2012-01-22 17:45:28 -0800

    hooking up basic template to format results

--- snac-launch.js
+++ snac-launch.js
@@ -6,14 +6,32 @@ SNAC.related = (typeof SNAC.related !== 'undefined') ? SNAC.related : {};
 
 /* the action happens from the bottom up in source order */
 
-
-SNAC.related.stash = (typeof SNAC.related.stash !== 'undefined') ? SNAC.related.stash : function(){
-  //
-  // alert("x");
+/* format the result with a {{moustashe}} template */
+SNAC.related.stash = (typeof SNAC.related.stash !== 'undefined') ? SNAC.related.stash : function(results){
+  // set up SNAC frame
+  template = '<div><div><a href="http://socialarchive.iath.virginia.edu/xtf/view?docId={{filename}}">{{identity}}</a></div>';
+  template += '{{#dbpedia}}{{dbpedia}}\n{{/dbpedia}}';
+  template += '{{#viaf}}{{viaf}}\n{{/viaf}}</div>';
+  var snac = $.mustache(template, results);
+  snac = $(snac);
+  snac.dialog({ 
+    autoOpen: false, 
+    title: 'related',
+    position: ['right', 'bottom']
+  }); 
+  // click handler
+  nd.click(function(){
+    if (!snac.dialog('isOpen')) { 
+      snac.dialog('open');
+    } else {
+      snac.dialog('close');
+    }
+  });
 };
 
-/* add the div with the snac trigger; check for tooltip */
-SNAC.related.addDiv = (typeof SNAC.related.addDiv !== 'undefined') ? SNAC.related.addDiv : function(){
+
+/* we have results! add a div to hold the button */
+SNAC.related.addDiv = (typeof SNAC.related.addDiv !== 'undefined') ? SNAC.related.addDiv : function(results){
   nd = $('<button id="logo-0fc2" title="related collections">&#x0FC2;</button>');
   nd.css({
     position: "fixed", 
@@ -26,41 +44,49 @@ SNAC.related.addDiv = (typeof SNAC.related.addDiv !== 'undefined') ? SNAC.relate
   });
   $('body').append(nd);
   nd.button();
-  // set up SNAC frame
-  snac = $('<div/>');
-  snac.dialog({ 
-    autoOpen: false, 
-    title: 'Archival Context',
-    position: ['right', 'bottom']
-  }); 
-  // click handler
-  nd.click(function(){
-    if (!snac.dialog('isOpen')) { 
-      snac.dialog('open');
-    } else {
-      snac.dialog('close');
-    }
-  });
 
   // nd.button({icons: {primary: 'ui-icon-circle-triangle-w'}});
   // jQuery and jQueryUI are set up here
-  if (typeof jQuery.tooltip == 'undefined') {
+  if (typeof jQuery.mustache == 'undefined') {
     jQuery.getScript('./jquery.mustache.js',function(){
-      SNAC.related.stash();
+      SNAC.related.stash(results);
     });
   } else {
-      SNAC.related.stash();
+      SNAC.related.stash(results);
   }
+};
+
+SNAC.related.grabURL = (typeof SNAC.related.grabURL !== 'undefined') ? SNAC.related.grabURL : function(){
+  // this just works for OAC; will need different searches for the different sources
+  return $('div.permlink a')[0].href;
+};
+
+SNAC.related.checkSNAC = (typeof SNAC.related.checkSNAC !== 'undefined') ? SNAC.related.checkSNAC : function(){
+  // URL of this page
+  // url = "http://www.oac.cdlib.org/findaid/ark:/13030/kt0f59q1h4"
+  url = SNAC.related.grabURL();  
+  // URL of the SNAC query
+  url = "http://archive1.village.virginia.edu:8012/rex/snac/indices/sourceEADurlIndex?key=creatorOf&value=" + url
+  // JSON to JSONP Bridge
+  // http://pipes.yahoo.com/pipes/pipe.info?_id=332d9216d8910ba39e6c2577fd321a6a
+  url = "http://pipes.yahoo.com/pipes/pipe.run?u="+ encodeURIComponent(url) + "&_id=332d9216d8910ba39e6c2577fd321a6a&_render=json&_callback=?"
+
+  // check if there is a result in SNAC
+  $.getJSON(url, function(data) {
+    if (data.value.items[0].results) {
+      SNAC.related.addDiv(data.value.items[0].results);
+    }
+  });
 };
 
 SNAC.related.setstuff = (typeof SNAC.related.setstuff !== 'undefined') ? SNAC.related.setstuff : function(){
   $('head').append('<link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.17/themes/base/jquery-ui.css" type="text/css" media="all" />');
   if (typeof jQuery.ui == 'undefined') {
     jQuery.getScript('http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min.js',function(){
-      SNAC.related.addDiv();
+      SNAC.related.checkSNAC();
     });
   } else {
-    SNAC.related.addDiv();
+    SNAC.related.checkSNAC();
   }
 };
 
